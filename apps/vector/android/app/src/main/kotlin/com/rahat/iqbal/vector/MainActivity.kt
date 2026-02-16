bd0d4dc399a87cd9f1ff77e87a91523a89831cdf
rahat.iqbal <rahat.iqbal@technonext.com>
2026-02-16 22:34:26 +0600

    chore: init flutter app

---
+++ apps/vector/android/app/src/main/kotlin/com/rahat/iqbal/vector/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rahat_iqbal.vector
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ apps/vector/lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:vector/app/app.dart';
+import 'package:vector/bootstrap.dart';
+
+Future<void> main() async {
+  await bootstrap(() => const App());
+}

36ef8be65fe790425ef6cf8b7e158b2380ef4433
Rahat Iqbal <iqbalrahatcse@outlook.com>
2026-03-12 15:17:35 +0600

    add GlobalDataResponse

---
+++ packages/data/lib/src/response_models/data/global_data_response.dart
@@ -0,0 +1,34 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'global_data_response.freezed.dart';
+part 'global_data_response.g.dart';
+
+@freezed
+abstract class GlobalDataResponse with _$GlobalDataResponse {
+  const factory GlobalDataResponse({
+    CryptoData? data,
+  }) = _$GlobalDataResponseImpl;
+
+  factory GlobalDataResponse.fromJson(Map<String, dynamic> json) => 
+      _$GlobalDataResponseFromJson(json);
+}
+
+@freezed
+abstract class CryptoData with _$CryptoData {
+  const factory CryptoData({
+    int? activeCryptocurrencies,
+    int? upcomingIcos,
+    int? ongoingIcos,
+    int? endedIcos,
+    int? markets,
+    Map<String, double>? totalMarketCap,
+    Map<String, double>? totalVolume,
+    Map<String, double>? marketCapPercentage,
+    double? marketCapChangePercentage24hUsd,
+    double? volumeChangePercentage24hUsd,
+    int? updatedAt,
+  }) = _$CryptoDataImpl;
+
+  factory CryptoData.fromJson(Map<String, dynamic> json) => 
+      _$CryptoDataFromJson(json);
+}

1e23510a7cc2dbc52a436a7b1f68d2ae919ab97e
Rahat Iqbal <81703704+Rahat-Iqbal-git@users.noreply.github.com>
2026-07-14 01:42:10 +0600

    feat: add AsyncState class

---
+++ packages/core/lib/src/state/async_state.dart
@@ -0,0 +1,23 @@
+sealed class AsyncState<T> {
+  const AsyncState();
+}
+
+final class AsyncInitial<T> extends AsyncState<T> {
+  const AsyncInitial();
+}
+
+final class AsyncLoading<T> extends AsyncState<T> {
+  const AsyncLoading();
+}
+
+final class AsyncSuccess<T> extends AsyncState<T> {
+  const AsyncSuccess(this.data);
+
+  final T data;
+}
+
+final class AsyncFailure<T> extends AsyncState<T> {
+  const AsyncFailure(this.message);
+
+  final String message;
+}
--- packages/core/lib/vector_core.dart
+++ packages/core/lib/vector_core.dart
@@ -1,3 +1,4 @@
 export 'src/failure/failure.dart';
 export 'src/config/environment_config.dart';
 export 'src/config/environment.dart';
+export 'src/state/async_state.dart';
